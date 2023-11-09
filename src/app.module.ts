c21e6c0898c0c8a084f896b5b5c553470ec7f2d5
vhoreho <70638703+vhoreho@users.noreply.github.com>
2023-11-09 15:07:55 +0300

    fix: ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { WalletReportModule } from './core/wallet-report/wallet-report.module';
       entities: [User, BlockChain],
       autoLoadEntities: true,
       synchronize: true,
-      ssl: { rejectUnauthorized: false },
+      ssl: true,
     }),
     BlockchainModule,
     WalletReportModule,
