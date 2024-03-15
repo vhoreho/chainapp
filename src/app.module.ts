0b17e61212188bf8784cb6fd146061e173cf980e
uhoreho <uladzislau.horeh@pargoo.com>
2024-03-15 20:49:48 +0300

    fix: db credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,25 +10,28 @@ import { WalletReportModule } from './core/wallet-report/wallet-report.module';
 import { ProfileModule } from './core/profile/profile.module';
 import { NewTransaction } from './core/blockchain/entities/new-transaction.entity';
 import { SignedTransaction } from './core/blockchain/entities/signed-transactions.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
-    ConfigModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'ec2-34-252-169-131.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      username: 'gceogbbjeavkuc',
-      password:
-        '445983d32512ee0ab5ac69ad76e488edd89d90ecdf7636623e6e661281a23a27',
-      database: 'da4toggk9aph9b',
-      entities: [User, Transaction, NewTransaction, SignedTransaction],
-      autoLoadEntities: true,
-      synchronize: true,
-      ssl: { rejectUnauthorized: false },
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'ec2-34-248-228-53.eu-west-1.compute.amazonaws.com',
+        port: 5432,
+        username: 'agjnaxebdiudbl',
+        password:
+          '3422595441cb01d6cf6e0c978bf65ec8194a163b99a187e28c9b5160a9389ff0',
+        database: 'd1ubp68n1ktqul',
+        entities: [User, Transaction, NewTransaction, SignedTransaction],
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
     }),
     BlockchainModule,
     WalletReportModule,
